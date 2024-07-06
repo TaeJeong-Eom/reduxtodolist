fc43d81ad5d62aa88c27a5f5f73c9170fcb03df3
eomthins <taejeong0416@gmail.com>
2024-07-06 18:32:45 +0900

    Remove sensitive information

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,3 @@ import './index.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// token: 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
-// git remote set-url origin https://TaeJeong-Eom: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@github.com/TaeJeong-Eom/React_todolist.git
